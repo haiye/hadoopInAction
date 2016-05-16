5e0786e40e81933a52d181776bba0bd74213c5d6
MichaelYe <haiye@paypal.com>
2016-05-16 15:25:22 +0800

    update WordCountChapter04E1 with combine class

--- src/main/java/cn/edu/ruc/cloudcomputing/book/chapter04/WordCountChapter04E1.java
+++ src/main/java/cn/edu/ruc/cloudcomputing/book/chapter04/WordCountChapter04E1.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -21,71 +19,122 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public class WordCountChapter04E1 extends Configured implements Tool {
-	static int static_int_map = 1;
-	static int static_int_reduce = 1;
+    static int static_int_map = 1;
+    static int static_int_reduce = 1;
 
-	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    public static class WordCountChapter04E1Map extends Mapper<LongWritable, Text, Text, Text> {
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
-			System.out.println("yehaimin_debug: mapTask number=" + static_int_map);
-			static_int_reduce++;
-			String line = value.toString(); // 将输入的纯文本文件的数据转化成String
-			System.out.println(line);// 为了便于程序的调试，输出读入的内容
-			// 将输入的数据先按行进行分割
-			StringTokenizer tokenizerArticle = new StringTokenizer(line, "\n");
-			// 分别对每一行进行处理
-			while (tokenizerArticle.hasMoreTokens()) {
-				// 每行按空格划分
-				StringTokenizer tokenizerLine = new StringTokenizer(tokenizerArticle.nextToken());
-				String strName = tokenizerLine.nextToken(); // 学生姓名部分
-				String strScore = tokenizerLine.nextToken();// 成绩部分
-				Text name = new Text(strName);// 学生姓名
-				int scoreInt = Integer.parseInt(strScore);// 学生成绩score of
-															// student
-				context.write(name, new IntWritable(scoreInt));// 输出姓名和成绩
-			}
-		}
-	}
+            int SUBJECT_COUNT = 1;
 
-	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException,
-				InterruptedException {
-			System.out.println("yehaimin_debug: reduceTask number=" + static_int_reduce);
-			static_int_reduce++;
-			int sum = 0;
-			int count = 0;
-			Iterator<IntWritable> iterator = values.iterator();
-			while (iterator.hasNext()) {
-				sum += iterator.next().get(); // 计算总分
-				count++;// 统计总的科目数
-			}
-			int average = (int) sum / count;// 计算平均成绩
-			context.write(key, new IntWritable(average));
-		}
-	}
+            System.out.println("WordCountChapter04E1Map_debug: mapTask number=" + (static_int_map++));
 
-	public int run(String[] args) throws Exception {
+            String line = value.toString(); // 将输入的纯文本文件的数据转化成String
+            System.out.println(line);// 为了便于程序的调试，输出读入的内容
+            // 将输入的数据先按行进行分割
+            StringTokenizer tokenizerArticle = new StringTokenizer(line, "\n");
+            // 分别对每一行进行处理
+            while (tokenizerArticle.hasMoreTokens()) {
 
-		Configuration conf = new Configuration();
-		Job job = new Job(getConf());
-		job.setJarByClass(WordCountChapter04E1.class);
-		job.setJobName("Score_Process");
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-		job.setMapperClass(Map.class);
-		job.setCombinerClass(Reduce.class);
-		job.setReducerClass(Reduce.class);
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
+                String next_str = tokenizerArticle.nextToken();
 
-		FileInputFormat.setInputPaths(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		boolean success = job.waitForCompletion(true);
-		return success ? 0 : 1;
-	}
+                // 每行按空格划分
+                StringTokenizer tokenizerLine = new StringTokenizer(next_str);
+                String nameStr = tokenizerLine.nextToken(); // 学生姓名部分
+                Text nameText = new Text(nameStr);// 学生姓名
+                
+                String scoreStr = tokenizerLine.nextToken();// 成绩部分
+                scoreStr = scoreStr + "~" + SUBJECT_COUNT;// scoreOfStudent~studentCount
+                Text scoreText = new Text(scoreStr);
 
-	public static void main(String[] args) throws Exception {
-		int ret = ToolRunner.run(new WordCountChapter04E1(), args);
-		System.exit(ret);
-	}
+                System.out.println("student=" + nameText + "; score=" + scoreText);
+
+                context.write(nameText, scoreText);// 输出姓名和成绩
+            }
+        }
+    }
+
+    public static class WordCountChapter04E1Combine extends Reducer<Text, Text, Text, Text> {
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            System.out.println("WordCountChapter04E1Combine_debug: student_name=" + key.toString());
+
+            int sum = 0;
+            int subjectCounts = 0;
+
+            Iterator<Text> iterator = values.iterator();
+
+            while (iterator.hasNext()) {
+                String scoreAndSubjectCounts = iterator.next().toString();
+                StringTokenizer scoreAndSubjectCountsStringTokenizer = new StringTokenizer(scoreAndSubjectCounts, "~");
+                while (scoreAndSubjectCountsStringTokenizer.hasMoreTokens()) {
+                    String scoreStr = scoreAndSubjectCountsStringTokenizer.nextToken();
+                    String subjectCountsStr = scoreAndSubjectCountsStringTokenizer.nextToken();
+                    sum += Integer.parseInt(scoreStr);
+                    subjectCounts += Integer.parseInt(subjectCountsStr);
+                    System.out.println("scoreStr=" + scoreStr + "; subjectCounts=" + subjectCounts + "; sum =" + sum);
+                }
+            }
+
+            /* main difference between combine and reduce */
+            context.write(key, new Text(sum + "~" + subjectCounts));
+        }
+    }
+
+    public static class WordCountChapter04E1Reduce extends Reducer<Text, Text, Text, Text> {
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            System.out.println("WordCountChapter04E1Reduce_debug: reduceTask number=" + (static_int_reduce++));
+
+            int sum = 0;
+            int subjectCounts = 0;
+
+            Iterator<Text> iterator = values.iterator();
+
+            while (iterator.hasNext()) {
+                String scoreAndSubjectCounts = iterator.next().toString();
+                StringTokenizer scoreAndSubjectCountsStringTokenizer = new StringTokenizer(scoreAndSubjectCounts, "~");
+                while (scoreAndSubjectCountsStringTokenizer.hasMoreTokens()) {
+                    String scoreStr = scoreAndSubjectCountsStringTokenizer.nextToken();
+                    String subjectCountsStr = scoreAndSubjectCountsStringTokenizer.nextToken();
+                    sum += Integer.parseInt(scoreStr);
+                    subjectCounts += Integer.parseInt(subjectCountsStr);
+                    System.out.println("scoreStr=" + scoreStr + "; subjectCounts=" + subjectCounts + "; sum =" + sum);
+                }
+            }
+
+            /* main difference between combine and reduce */
+            int average = sum / subjectCounts;// 计算平均成绩
+            context.write(key, new Text(average + ""));
+
+        }
+    }
+
+    public int run(String[] args) throws Exception {
+
+        Job job = Job.getInstance(getConf());
+        job.setJarByClass(WordCountChapter04E1.class);
+        job.setJobName("WordCountChapter04E1");
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        job.setInputFormatClass(TextInputFormat.class);
+
+        job.setMapperClass(WordCountChapter04E1Map.class);
+
+        job.setCombinerClass(WordCountChapter04E1Combine.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        job.setReducerClass(WordCountChapter04E1Reduce.class);
+
+        job.setOutputFormatClass(TextOutputFormat.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        boolean success = job.waitForCompletion(true);
+        return success ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int ret = ToolRunner.run(new WordCountChapter04E1(), args);
+        System.exit(ret);
+    }
 }
